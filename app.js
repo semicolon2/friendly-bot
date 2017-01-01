858d9042a90cac3a124178a32158960ae6768b14
semicolon2 <benfallan12@gmail.com>
2017-01-01 01:14:49 -0700

    ../

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/quotes', function (req, res) {
-    res.sendFile('quotes.json');
+    res.sendFile('../quotes.json');
 });
 
 //================bot & bot logic===============================================
